eea0e645624a1f3f00b565b94283d82afce23e3a
Norna <raingsei.hai@gmail.com>
2016-05-20 01:26:04 +0200

    release 1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup (
 	name = "molnframework",
-	version = "1.2",
+	version = "1.3",
     install_requires = ['psutil==4.1.0','Django>=1.9.4'],
 	packages = find_packages(exclude=["samples","samples.*","sample","sample.*"]),
     author = "rhinodiana",
